a0c197ec93c5a5494e5e192fdbcdccf2bdd9ca13
leocasella2777-creator <leocasella2777@gmail.com>
2026-04-27 14:46:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,11 @@
-const CACHE = 'sistema-italia-v119';
-const FILES = [
-  './', './index.html', './debito.html', './prefazione.html', './fiscale.html',
-  './bilancio.html', './pensioni.html', './energia.html',
-  './welfare.html', './proiezioni.html', './politica.html',
-  './riflessioni.html', './principi.html', './fonti.html',
-  './manifest.json', './icon-192.svg', './icon-512.svg',
-  './sistema_italia_v119.pdf'
-];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+// Service worker disabilitato
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.open(CACHE).then(cache=>
-    fetch(e.request).then(res=>{cache.put(e.request,res.clone());return res;})
-    .catch(()=>caches.match(e.request))
-  ));
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
